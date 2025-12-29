b8384e3d57abd83df0dc5b6dfac9342bf2ed28b1
Pao Zambrano <pao.zambranoo_@hotmail.com>
2025-12-29 15:47:47 -0500

    Add login steps for user authentication in Behave

---
+++ features/steps/login_steps.py
@@ -0,0 +1,16 @@
+from behave import given, when, then
+from pages.login_page import LoginPage
+
+@given('I navigate to the login page')
+def step_impl(context):
+    context.login_page = LoginPage(context.driver)
+    context.login_page.visit("https://automationexercise.com/login")
+
+@when('I enter email "{email} and password {password}')
+def step_impl(context, email, password):
+    context.login_page.login(email, password)
+
+@then('I should see "Logged in as" on the navbar')
+def step_impl(context):
+    error_text = context.login_page.get_text(context.login_page.ERROR_MESSAGE)
+    assert "your email or password is incorrect" in error_text.lower()
